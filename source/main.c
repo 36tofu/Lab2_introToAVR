c525c3a66e1312addb186e69368114d29dfb40ff
36tofu <christopher99chen@gmail.com>
2021-01-12 20:45:16 -0800

    Fixed main.c errors

--- source/main.c
+++ source/main.c
@@ -10,23 +10,18 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#endif
+#endif	
 
 int main(void) {
-	DDRB = 0xFF; // Configure port B's 8 pins as outputs
-	PORTB = 0x00; // Initialize PORTB output to 0’s
-	
-	unsigned char temp_input = 0x00; // Init output on PORTB to 0x00
-
-	while(1){
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+				// Initialize output on PORTB to 0x00
+	unsigned char temp_input = 0x00;
+	while(1) {
 		temp_input = PINA;
-		PORTB = temp_input;
+		PORTB = temp_input;	// Writes port B's 8 pins with the values
+				// on port A's 8 pins
 	}
 	return 1;
-
-    /* Insert your solution below */
-    while (1) {
-
-    }
-    return 1;
 }
+
